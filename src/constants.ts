a728238b9dd5158c2d57a1f055697f2d03a735ce
zy0n.bear <zy0n@users.noreply.github.com>
2025-03-14 13:30:56 +0700

    Add newline for improved readability in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ const ADDRESS_LENGTH_LIMIT = 127;
 const ALL_CHAINS_NETWORK_ID = new Uint8Array(8).fill(0xff);
 const CURRENT_ADDRESS_VERSION = 0x01;
 const PREFIX = "0zk";
+
 // This is the ASCII representation of the string "railgun\0"
 const RAILGUN_ASCII = new Uint8Array([114, 97, 105, 108, 103, 117, 110, 0]);
 
