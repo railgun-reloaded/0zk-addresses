71f771fd65785d45c75ab30f59be3e7c0e5e8a70
Matt Grote <matiasgrote@gmail.com>
2025-03-17 14:39:14 +0700

    remove unused types

--- src/types.ts
+++ src/types.ts
@@ -16,16 +16,3 @@ export type AddressData = {
   chain?: Optional<Chain>;
   version?: Optional<number>;
 };
-
-export type BytesData = bigint | number | ArrayLike<number> | string;
-
-export enum ByteLength {
-  UINT_8 = 1,
-  UINT_56 = 7,
-  UINT_120 = 15,
-  UINT_128 = 16,
-  Address = 20,
-  UINT_192 = 24,
-  UINT_248 = 31,
-  UINT_256 = 32,
-}
