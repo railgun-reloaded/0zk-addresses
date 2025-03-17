b9e0e9a1b9b050dae27c7dab295ce7078f87973d
zy0n.bear <zy0n@users.noreply.github.com>
2025-03-17 14:59:47 +0700

    Refactor tests to use TEST_VECTORS and add new tests for RailgunAddressV2 parsing and stringifying

--- test/0zkaddress.test.ts
+++ test/0zkaddress.test.ts
@@ -1,62 +1,17 @@
 import { describe, it } from "node:test";
-import {
-  ChainType,
-  type AddressData,
-  type RailgunAddressLike,
-} from "../src/types";
+import { type AddressData, type RailgunAddressLike } from "../src/types";
 import expect from "expect";
 
-import { parse, stringify } from "../src";
+import { parse, stringify } from "../src/V2/actions";
 import { ADDRESS_LENGTH_LIMIT } from "../src/constants";
+import { TEST_VECTORS } from "./vectors";
 
 describe("bech32-encode2", () => {
   it("Should encode and decode addresses", () => {
-    const vectors = [
-      {
-        pubkey: new Uint8Array([
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ]),
-        chain: { type: ChainType.EVM, id: 1 },
-        address:
-          "0zk1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqunpd9kxwatwqyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhshkca",
-        version: 1,
-      },
-      {
-        pubkey: new Uint8Array([
-          0, 0, 1, 191, 213, 104, 28, 4, 121, 190, 154, 142, 248, 221, 139, 170,
-          221, 151, 17, 88, 153, 169, 175, 48, 179, 210, 69, 88, 67, 175, 180,
-          27,
-        ]),
-        chain: { type: ChainType.EVM, id: 56 },
-        address:
-          "0zk1qyqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pkunpd9kxwatw8qqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pkcsu8tp",
-        version: 1,
-      },
-      {
-        pubkey: new Uint8Array([
-          0, 0, 1, 191, 213, 104, 28, 4, 121, 190, 154, 142, 248, 221, 139, 170,
-          221, 151, 17, 88, 153, 169, 175, 48, 179, 210, 69, 88, 67, 175, 180,
-          27,
-        ]),
-        chain: { type: 1, id: 56 },
-        address:
-          "0zk1qyqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pkumpd9kxwatw8qqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pkwrfm4m",
-        version: 1,
-      },
-      {
-        pubkey: new Uint8Array([
-          238, 107, 76, 112, 47, 128, 112, 200, 221, 234, 28, 187, 139, 15, 106,
-          74, 81, 139, 119, 250, 141, 63, 155, 104, 97, 123, 102, 69, 80, 231,
-          95, 100,
-        ]),
-        chain: undefined,
-        address:
-          "0zk1q8hxknrs97q8pjxaagwthzc0df99rzmhl2xnlxmgv9akv32sua0kfrv7j6fe3z53llhxknrs97q8pjxaagwthzc0df99rzmhl2xnlxmgv9akv32sua0kg0zpzts",
-        version: 1,
-      },
-    ];
-    for (const [_, { pubkey, chain, version, address }] of vectors.entries()) {
+    for (const [
+      _,
+      { pubkey, chain, version, address },
+    ] of TEST_VECTORS.entries()) {
       const addressData: AddressData = {
         masterPublicKey: pubkey,
         viewingPublicKey: pubkey,
