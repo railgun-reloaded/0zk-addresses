a7ba7d129ea1bdfb45a49e26715f04a8c9ac42eb
zy0n.bear <zy0n@users.noreply.github.com>
2025-03-18 12:13:13 +0700

    Update test vectors to use proper ChainType enum for chain examples

--- test/codec.test.ts
+++ test/codec.test.ts
@@ -25,19 +25,19 @@ const testVectors = [
       0, 0, 1, 191, 213, 104, 28, 4, 121, 190, 154, 142, 248, 221, 139, 170,
       221, 151, 17, 88, 153, 169, 175, 48, 179, 210, 69, 88, 67, 175, 180, 27,
     ]),
-    chain: { type: 1, id: BigInt(56) },
+    chain: { type: ChainType.BSC, id: BigInt(56) },
     address:
       "0zk1qyqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pkumpd9kxwatw8qqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pkwrfm4m" as RailgunAddressLike,
     version: 1,
   },
   {
     pubkey: new Uint8Array([
-      0, 0, 1, 191, 213, 104, 28, 4, 121, 190, 154, 142, 248, 221, 139, 170,
-      221, 151, 17, 88, 153, 169, 175, 48, 179, 210, 69, 88, 67, 175, 180, 27,
+      238, 107, 76, 112, 47, 128, 112, 200, 221, 234, 28, 187, 139, 15, 106, 74,
+      81, 139, 119, 250, 141, 63, 155, 104, 97, 123, 102, 69, 80, 231, 95, 100,
     ]),
-    chain: { type: ChainType.ANY, id: BigInt(0) },
+    chain: { type: ChainType.ANY, id: BigInt(CHAIN_ID_ANY) },
     address:
-      "0zk1qyqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476phrtpd9kxwatwqqqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pk6xnyq6" as RailgunAddressLike,
+      "0zk1q8hxknrs97q8pjxaagwthzc0df99rzmhl2xnlxmgv9akv32sua0kfrv7j6fe3z53llhxknrs97q8pjxaagwthzc0df99rzmhl2xnlxmgv9akv32sua0kg0zpzts" as RailgunAddressLike,
     version: 1,
   },
 ];
